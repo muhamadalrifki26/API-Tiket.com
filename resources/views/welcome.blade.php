465823667e2bdf3a0ecd95bd863bbe7ba713626c
CyMas <kikinion26@gmail.com>
2016-05-19 14:37:06 +0700

    selesai

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="tiket">API Project</div>
+@endsection
+<style type="text/css">
+.tiket{
+    font-family: sans-serif;
+    font-size: 30pt;
+    color: #3498db;
+    text-align: center;
+    margin-top: 50px;
+}
+.sh{
+    font-family: sans-serif;
+    font-size: 10pt;
+    color: #3498db;
+    text-align: center;
+    margin-top: 10px;
+}
+</style>
